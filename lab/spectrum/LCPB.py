99b41aedf66626b9227192b27938b006b588ba3f
jco25 <jco25@277ba3e2-0388-4110-9b48-a7b1bf2ca1c3>
2009-03-03 21:18:30 +0000

    Math appears correct. Consider implementing the genlaguerre stuff recursively using the relations in Jens' book
    git-svn-id: file:///home/jacko/Downloads/LAB_REPO@10 277ba3e2-0388-4110-9b48-a7b1bf2ca1c3

--- lab/spectrum/LCPB.py
+++ lab/spectrum/LCPB.py
@@ -5,6 +5,14 @@ from scipy import array, sqrt, exp, pi, factorial, cos, sin
 from scipy.linalg import eig
 from scipy.special import genlaguerre
 
+####
+#### Create a function that gives an array of genlaguerres.
+#### it will need EL and EC as parameters (for phi0), in
+#### addition to a size
+#### 
+####
+
+
 def hamiltonian(size, EC, EJ, EL, flux):
     hbar_w0 = sqrt( 8. * EL * EC )
     phi0 = ( 8. * EC / EL ) ** .25 
@@ -18,7 +26,7 @@ def hamiltonian(size, EC, EJ, EL, flux):
                 a[row][col] += hbar_w0 * (row+0.5)
             #the nonzero cosine elements
             if (col-row)%2==0:
-                n = row if row<col else col
+                n = min(row,col)
                 m = abs(col-row)/2 # because of Hermitianness
                 a[row][col] += \
                     -EJ * cos(2*pi*flux/flux0) * (-2)**-m \
@@ -28,13 +36,13 @@ def hamiltonian(size, EC, EJ, EL, flux):
             #the nonzero sine elements
             else:
                 ### IS THIS PART RIGHT?
-                n = row if row<col else col
+                n = min(row,col)
                 m = (abs(col-row)-1)/2
                 a[row][col] += \
-                    EJ * sin(2*pi*flux/flux0) * (-2)**-m \
+                    -EJ * sin(2*pi*flux/flux0) * (-2)**(-m) * 2**-.5 \
                     * sqrt(factorial(n)/factorial(n+2*m+1)) \
                     * phi0**(2*m+1) * exp(phi0**2/-4) \
-                    * genlaguerre(n,2*m+1)(phi0**2/2) ## NEGATIVES CANCEL HERE???
+                    * genlaguerre(n,2*m+1)(phi0**2/2) ## Check overall signs
             
     return a
 
@@ -54,7 +62,7 @@ def main():
     EL = 1
     FLUX = .25
 
-    matrix_size = 4
+    matrix_size = 100
     num_levels = 5
     
     print "Populating Matrix..."
